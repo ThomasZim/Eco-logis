79dca269fadf5ad3baaabd7ee561b50f3c16df0c
Thomas Zimmermann <34342756+ThomasZim@users.noreply.github.com>
2023-11-02 09:34:58 +0100

    Add trigger event and movements

--- Eco-logis/Assets/Scripts/Events.cs
+++ Eco-logis/Assets/Scripts/Events.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Events : MonoBehaviour
 {
+    public GameObject MainCharacter;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,12 @@ public class Events : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == MainCharacter) {
+            Debug.Log("Collision!");
+        }
     }
 }
--- Eco-logis/Assets/Scripts/Movements.cs
+++ Eco-logis/Assets/Scripts/Movements.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Movements : MonoBehaviour
 {
+    // Public variables
+    [Range(0f, 0.02f)]
+    public float CharacterSpeed = 0.01f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,23 @@ public class Movements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newLocalPos = transform.localPosition;
+
+        if (Input.GetKey(KeyCode.A)) {
+            newLocalPos.x -= CharacterSpeed;
+            transform.localPosition = newLocalPos;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            newLocalPos.x += CharacterSpeed;
+            transform.localPosition = newLocalPos;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            newLocalPos.z += CharacterSpeed;
+            transform.localPosition = newLocalPos;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            newLocalPos.z -= CharacterSpeed;
+            transform.localPosition = newLocalPos;
+        }
     }
 }
