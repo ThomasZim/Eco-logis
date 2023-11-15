b224f6f47910645f6b3e08234ef46130efa6f4e5
Thomas Zimmermann <thozim438@gmail.com>
2023-11-15 15:11:06 +0100

    Add player movement, interaction and transition

---
+++ Eco-logis/Assets/Scripts/DoorEvent.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            Debug.Log("Player entered the Door");
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            LevelLoader levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+            levelLoader.LoadNextLevel();
+        }
+    }
+}
--- Eco-logis/Assets/Scripts/LevelLoader.cs
+++ Eco-logis/Assets/Scripts/LevelLoader.cs
@@ -13,10 +13,7 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            LoadNextLevel();
-        }
+
     }
 
     public void LoadNextLevel()
