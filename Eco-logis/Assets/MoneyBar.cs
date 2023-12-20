d2f4862bd764225a7e8c27418ba390625b6c03b4
Valérie Pompili <valeriepompili@hotmail.com>
2023-12-20 18:56:21 +0100

    Add menu add

---
+++ Eco-logis/Assets/MoneyBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyBar : MonoBehaviour
+{
+     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1F);
+
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
